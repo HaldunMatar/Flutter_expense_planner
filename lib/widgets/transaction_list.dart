1fc6d1d7f711f5e4b0531c07fb475a563b7d22a3
unknown <kh82matar@gmail.com>
2021-03-31 19:20:05 +0300

    expense_planner_adaptive_resposive

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -16,7 +16,7 @@ class TransactionList extends StatelessWidget {
             children: <Widget>[
               Text(
                 'No transactions added yet!',
-                style: Theme.of(context).textTheme.title,
+                style: Theme.of(context).textTheme.headline6,
               ),
               SizedBox(
                 height: 20,
@@ -49,7 +49,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   title: Text(
                     transactions[index].title,
-                    style: Theme.of(context).textTheme.title,
+                    style: Theme.of(context).textTheme.headline6,
                   ),
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
